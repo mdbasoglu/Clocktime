0030bc9c8a05118e39375c613efc4ec3d00f4b53
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2021-12-18 08:31:47 +0100

    erstesCommit

---
+++ Uebung54_Clocktime_Musterlösung/Clocktime.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Uebung54_Clocktime_Musterlösung
+{
+    class Clocktime
+    {
+        private int totalSeconds;
+        private const int SecondsADay = 86400;
+
+
+        public int Hours { get => totalSeconds / 3600; }
+        public int Minutes { get => totalSeconds / 60 % 60; }
+        public int Seconds { get => totalSeconds % 60; }
+
+
+        public Clocktime(int h, int m, int s)
+        {
+            totalSeconds = h * 3600 + m * 60 + s;
+            NormalizeClocktime();
+        }
+
+        private void NormalizeClocktime()
+        {
+            totalSeconds %= SecondsADay;
+
+            if (totalSeconds < 0)
+            {
+                totalSeconds += SecondsADay;
+            }
+        }
+
+        public Clocktime(int h, int m) : this(h, m, 0)
+        {
+
+        }
+
+        public Clocktime(int h) : this(h, 0, 0)
+        {
+
+        }
+
+        public Clocktime() : this(0, 0, 0)
+        {
+            //oder können wir anstatt this totalSeconds=0; nutzen
+        }
+
+        public Clocktime(Clocktime ct) //Copy Konstruktor
+        {
+            totalSeconds = ct.totalSeconds;
+        }
+
+        public Clocktime Add(int s)
+        {
+            totalSeconds = totalSeconds + s;
+            NormalizeClocktime();
+
+            return this;
+
+        }
+
+        public int Diff(Clocktime ct)
+        {
+            int diffsec = ct.totalSeconds - totalSeconds;
+
+            if (diffsec < 0)
+            {
+                diffsec += SecondsADay;
+            }
+
+            return diffsec;
+        }
+
+        public override string ToString()
+        {
+            //hh:mm:ss
+            return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+        }
+
+
+
+
+    }
+}
---
+++ Uebung54_Clocktime_Musterlösung/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uebung54_Clocktime_Musterlösung
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Clocktime ct = new Clocktime(8, 10, 40);
+            Console.WriteLine(ct.Hours);
+            Console.WriteLine(ct.Minutes);
+            Console.WriteLine(ct.Seconds);
+
+            Console.WriteLine(ct);
+
+            Clocktime c1 = new Clocktime().Add(5);
+            Console.WriteLine(c1);
+
+            Clocktime c2 = new Clocktime(1, 7, -101).Add(-8000);
+            Console.WriteLine(c2);
+
+            Console.WriteLine(c1.Diff(c2));
+            Console.WriteLine(c2.Diff(c1));
+
+
+
+            Clocktime test = new Clocktime(-1, -5, 0);
+            Console.WriteLine(test);
+
+
+        }
+    }
+}
